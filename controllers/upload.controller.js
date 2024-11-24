b678217a0cf3fc9415271781e5fe5ad2c32ecdfc
carloscordova.dev <cordovamanuel22@icloud.com>
2024-11-24 08:50:57 -0800

    refactor: :art: formated code

--- controllers/upload.controller.js
+++ controllers/upload.controller.js
@@ -1,16 +1,13 @@
-import { console } from "inspector";
-
 
 const upload = async (req, res) => {
-  console.log("req", req.files)
-  try {
 
+  try {
 
     if (!req.files) {
 
       return res.status(400).json({ message: 'No file uploaded' });
     }
-    
+
 
     res.status(200).json("files  successfully uploaded ");
   } catch (error) {
