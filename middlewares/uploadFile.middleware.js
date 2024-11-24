3d4456a2faab887146976b04eced8a3c40238978
carloscordova.dev <cordovamanuel22@icloud.com>
2024-11-24 08:49:59 -0800

    feat: :sparkles: introduce custom file name

--- middlewares/uploadFile.middleware.js
+++ middlewares/uploadFile.middleware.js
@@ -1,4 +1,5 @@
 import multer from "multer";
+import { v4 as uuidv4 } from 'uuid';
 
 // //! single file upload 
 // // Configure multer for file uploads
@@ -9,10 +10,10 @@ import multer from "multer";
 //? ________________________________________________________________
 //! multiple files upload at the same time
 // Configure multer for file uploads
-const upload = multer({ dest: 'uploads/' });
+// const upload = multer({ dest: 'uploads/' });
 
-// Specify 'file' as the field name for the uploaded file and limit the upload to 2 files
-const uploadMultipleFiles = upload.array('file', 2);
+// // Specify 'file' as the field name for the uploaded file and limit the upload to 2 files
+// const uploadMultipleFiles = upload.array('file', 2);
 
 //! multiple fields upload at the same time in case we have avatar or resume upload file
 // // Configure multer for file uploads
@@ -26,6 +27,24 @@ const uploadMultipleFiles = upload.array('file', 2);
 // // Specify 'file' as the field name for the uploaded file and limit the upload to 2 files
 // const uploadSingleFile = multiUpload;
 
+
+// ----------------------------------------------------------------
+//! multiple files upload at the same time with custom name 
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'uploads/');
+  },
+  filename: (req, file, cb) => {
+    const { originalname } = file;
+    cb(null, `${uuidv4()}-${originalname}`);
+  }
+})
+// Configure multer for file uploads
+const upload = multer({ storage });
+
+// Specify 'file' as the field name for the uploaded file and limit the upload to 2 files
+const uploadMultipleFilesWithCustomName = upload.array('file', 2);
 export {
-  uploadMultipleFiles,
+  uploadMultipleFilesWithCustomName
 };
