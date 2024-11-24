37d3be2a0f26422dba29b57559ad138713f4abb9
carloscordova.dev <cordovamanuel22@icloud.com>
2024-11-24 07:13:21 -0800

    feat: :sparkles: begin project

---
+++ index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 3001;
+
+
+const app = express();
+
+app.use(express.json());
+
+// Routes
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
