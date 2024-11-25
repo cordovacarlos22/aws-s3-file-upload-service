e3bcb1b3a36c9b918f71db72890ce2304daf3196
carloscordova.dev <cordovamanuel22@icloud.com>
2024-11-25 12:29:49 -0800

    feat: :sparkles: introduce v3 of aws sdk with multiple file upload and return urls for each file uploaded

--- config/s3Service.js
+++ config/s3Service.js
@@ -1,6 +1,10 @@
-import AWS from "aws-sdk";
-import dotenv from 'dotenv';
+// import AWS from "aws-sdk";
+import dotenv from "dotenv";
 import { v4 as uuidv4 } from 'uuid';
+import {
+  PutObjectCommand,
+  S3Client
+} from "@aws-sdk/client-s3";
 
 dotenv.config();
 
@@ -47,6 +51,65 @@ dotenv.config();
 // export default s3UploadV2;
 
 // ! new version v3 
+
+
+// Function to upload  single file to S3 single file using v3
+// const s3UploadV3 = async (file) => {
+
+//   const s3Client = new S3Client();
+
+//   const awsBucktName = process.env.AWS_BUCKET_NAME
+//   const key = `upload/${uuidv4()}-${file.originalname}`;
+//   const param = {
+//     Bucket: awsBucktName ,
+//     Key: key,
+//     Body: file.buffer
+//   }
+//   // const result = s3Client.send(new PutObjectCommand(param));
+
+//   const result = await s3Client.send(new PutObjectCommand(param));
+//   return {
+//     result,
+//     url: `https://${awsBucktName}.s3.us-west-1.amazonaws.com/${key}`,
+//   }
+// };
+const s3UploadV3 = async (files) => {
+
+  const s3Client = new S3Client();
+
+  const awsBucktName = process.env.AWS_BUCKET_NAME
+
+  
+
+  // Map each file to a promise that uploads and returns the result with its URL
+  const results = await Promise.all(
+    files.map(async (file) => {
+      const key = `upload/${uuidv4()}-${file.originalname}`; // Generate unique key
+
+      const params = {
+        Bucket: awsBucktName,
+        Key: key,
+        Body: file.buffer,
+      };
+
+      // Upload file to S3
+      const result = await s3Client.send(new PutObjectCommand(params));
+
+      // Attach the public URL to the result
+      return {
+        ...result, // AWS response metadata
+        url: `https://${awsBucktName}.s3.us-west-1.amazonaws.com/${key}`,
+      };
+    })
+
+  );
+
+  return results; // Array of results, each with its corresponding URL
+};
+
+
+
+export default s3UploadV3;
 
 
 
