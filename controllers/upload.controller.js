26bf1528194a1e1f89689cf107d172304ee17f86
carloscordova.dev <cordovamanuel22@icloud.com>
2024-11-25 12:57:48 -0800

    feat: :boom: introduce create POST and retrive image url from aws s3 bucket

--- controllers/upload.controller.js
+++ controllers/upload.controller.js
@@ -1,5 +1,5 @@
 import s3UploadV3 from "../config/s3Service.js";
-import s3UploadV2 from "../config/s3Service.js";
+import Post from "../models/post.model.js";
 
 // !single file uploadv2 
 // const upload = async (req, res) => {
@@ -81,12 +81,21 @@ const upload = async (req, res) => {
     }
 
     const result = await s3UploadV3(req.files) // store the result 
-    console.log("result", result)
+    // Create a new post in your MongoDB database using the result data
+    // You can add additional fields to the post model as needed. For example:
+    // post.title = req.body.title
+    // post.content = req.body.content
+    // post.image = result.urls[0] // Assuming you have an array of URLs in the result object
+    // const post = new Post(req.body)
+    // await post.save()
+    const post = await Post.create({
+      ...req.body,
+      image: result.urls, // Guarda las URLs en el campo `image`
+    });
 
-    // Send the response with a proper JSON object
     res.status(200).json({
-      message: "File successfully uploaded",
-      data: result,
+      message: "Post created successfully",
+      post,
     });
   } catch (error) {
     res.status(500).json({ error: "Internal Server Error", details: error.message });
