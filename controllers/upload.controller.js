4cd3c2425ed6f03b136ba6f4831563248865f630
carloscordova.dev <cordovamanuel22@icloud.com>
2024-11-25 12:30:50 -0800

    feat: :sparkles: add controller to handle multiple file request

--- controllers/upload.controller.js
+++ controllers/upload.controller.js
@@ -1,6 +1,7 @@
+import s3UploadV3 from "../config/s3Service.js";
 import s3UploadV2 from "../config/s3Service.js";
 
-// !single file upload
+// !single file uploadv2 
 // const upload = async (req, res) => {
 
 //   try {
@@ -23,21 +24,70 @@ import s3UploadV2 from "../config/s3Service.js";
 
 // }
 
-//! multiple file upload
+//! multiple file upload v2 
+// const upload = async (req, res) => {
+
+//   try {
+
+
+//     if (!req.files) {
+
+//       return res.status(400).json({ message: 'No files uploaded' });
+//     }
+
+//     const results = await s3UploadV2(req.files) // store the result 
+
+
+//     res.status(200).json("files  successfully uploaded ", results);
+//   } catch (error) {
+//     res.status(500).json({ error: "Internal Server Error", details: error.message });
+//   }
+
+// }
+
+// !single file upload v3 
+// const upload = async (req, res) => {
+
+//   try {
+
+//     if (!req.files) {
+
+//       return res.status(400).json({ message: 'No file uploaded' });
+//     }
+
+//     const file = req.files[0]; // one grab the first file we received
+
+//     const result = await s3UploadV3(file) // store the result 
+//     console.log("result", result)
+
+//     // Send the response with a proper JSON object
+//     res.status(200).json({
+//       message: "File successfully uploaded",
+//       data: result,
+//     });
+//   } catch (error) {
+//     res.status(500).json({ error: "Internal Server Error", details: error.message });
+//   }
+
+// }
+//! multiple files upload v3 
 const upload = async (req, res) => {
 
   try {
 
-
     if (!req.files) {
 
-      return res.status(400).json({ message: 'No files uploaded' });
+      return res.status(400).json({ message: 'No file uploaded' });
     }
 
-    const results = await s3UploadV2(req.files) // store the result 
+    const result = await s3UploadV3(req.files) // store the result 
+    console.log("result", result)
 
-
-    res.status(200).json("files  successfully uploaded ", results);
+    // Send the response with a proper JSON object
+    res.status(200).json({
+      message: "File successfully uploaded",
+      data: result,
+    });
   } catch (error) {
     res.status(500).json({ error: "Internal Server Error", details: error.message });
   }
